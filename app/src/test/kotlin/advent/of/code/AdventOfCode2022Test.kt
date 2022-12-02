bf2dcfc4e0c754034911f2b7688f0a05998147d0
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-12-02 11:37:42 +0100

    day1 : step 2

--- app/src/test/kotlin/advent/of/code/AdventOfCode2022Test.kt
+++ app/src/test/kotlin/advent/of/code/AdventOfCode2022Test.kt
@@ -1,6 +1,5 @@
 package advent.of.code
 
-import com.google.common.collect.ImmutableMap
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -21,30 +20,57 @@ class AdventOfCode2022Test {
         }
 
         @Test
-        fun `_puzzle_ - Should read the raw metrics and return max calories carried by an elf`(){
+        fun `_puzzle_part_1_ - Should read the raw metrics and return max calories carried by an elf`(){
             val filePath = "src/test/resources/2022/day1"
             val caloriesMeasurements = BufferedReader(FileReader("$filePath/puzzleInput.txt")).lines().toList()
             val allMeasurements = computeCaloriesCarriedByEachElf(caloriesMeasurements)
             val maxCalories = allMeasurements.entries.maxOf { it.value }
             Assertions.assertThat(maxCalories).isEqualTo(72017)
         }
-    }
 
-    private fun computeCaloriesCarriedByEachElf(caloriesMeasurements: List<String>): Map<Int, Int> {
-        val allMeasurements = mutableMapOf<Int, Int>()
-        var currentElfIndex = caloriesMeasurements.indexOfFirst { it.isEmpty() }.plus(1)
-        var accumulator = 0
-        caloriesMeasurements.forEach { rawMeasurement ->
-            if (!rawMeasurement.isEmpty()) {
-                accumulator += rawMeasurement.toInt()
-            } else {
-                allMeasurements[currentElfIndex] = accumulator
-                accumulator = 0
-                currentElfIndex++
-            }
+        @Test
+        fun `_sample_part_2 total calories carried by the top three elves carrying the most calories `(){
+            val filePath = "src/test/resources/2022/day1"
+            val caloriesMeasurements = BufferedReader(FileReader("$filePath/sample.txt")).lines().toList()
+            val allMeasurements: Map<Int, Int> = computeCaloriesCarriedByEachElf(caloriesMeasurements)
+            val totalCaloriesForTheTopThree = allMeasurements.entries.sortedByDescending { it.value }
+            val maxCalories = totalCaloriesForTheTopThree.take(3).sumOf { it.value }
+            Assertions.assertThat(maxCalories).isEqualTo(45000)
         }
-        return allMeasurements
+
+        @Test
+        fun `_puzzle_part_2 total calories carried by the top three elves carrying the most calories `(){
+            val filePath = "src/test/resources/2022/day1"
+            val caloriesMeasurements = BufferedReader(FileReader("$filePath/puzzleInput.txt")).lines().toList()
+            val allMeasurements: Map<Int, Int> = computeCaloriesCarriedByEachElf(caloriesMeasurements)
+            val totalCaloriesForTheTopThree = allMeasurements.entries.sortedByDescending { it.value }
+            val totalCarriedByTopThreeElves = totalCaloriesForTheTopThree.take(3).sumOf { it.value }
+            Assertions.assertThat(totalCarriedByTopThreeElves).isEqualTo(212520)
+        }
+
+
+
+        private fun computeCaloriesCarriedByEachElf(caloriesMeasurements: List<String>): Map<Int, Int> {
+            val allMeasurements = mutableMapOf<Int, Int>()
+            var currentElfIndex = caloriesMeasurements.indexOfFirst { it.isEmpty() }.plus(1)
+            var accumulator = 0
+            caloriesMeasurements.forEach { rawMeasurement ->
+                if (rawMeasurement.isNotEmpty() && !rawMeasurement.isLastElement(caloriesMeasurements) ) {
+                    accumulator += rawMeasurement.toInt()
+                }else{
+                    val sumOfCalories = if(rawMeasurement.isLastElement(caloriesMeasurements)) rawMeasurement.toInt() else accumulator
+                    allMeasurements[currentElfIndex] = sumOfCalories
+                    accumulator = 0
+                    currentElfIndex++
+                }
+            }
+            return allMeasurements
+        }
     }
+}
 
-
+private fun String.isLastElement(caloriesMeasurements: List<String>): Boolean {
+    val indexOfCurrentMeasure = caloriesMeasurements.indexOf(this)
+    val lastElement = caloriesMeasurements.indexOfLast { it.isNotEmpty() }
+    return indexOfCurrentMeasure==lastElement
 }
