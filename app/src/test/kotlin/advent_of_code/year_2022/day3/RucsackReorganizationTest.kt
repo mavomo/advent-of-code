b671f292e7c997026d03f7e12eeed332ebf91aea
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-12-03 23:33:12 +0100

    day3 - sample - finding the common item between both compartiments

--- app/src/test/kotlin/advent_of_code/year_2022/day3/RucsackReorganizationTest.kt
+++ app/src/test/kotlin/advent_of_code/year_2022/day3/RucsackReorganizationTest.kt
@@ -1,5 +1,38 @@
 package advent_of_code.year_2022.day3
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 
-internal class RucsackReorganizationTest
+internal class RucsackReorganizationTest {
+
+    @Test
+    fun `should find the common item in both compartments `() {
+        val contentOfFirstRucksack = "vJrwpWtwJgWrhcsFMMfFFhFp"
+        val contentOfSecondRucksack = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+        val contentOfThirdRucksack = "PmmdzqPrVvPwwTWBwg"
+        val contentOfFourthRucksack = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+        val contentOfFifthRucksack = "ttgJtRGJQctTZtZT"
+        val contentOfSixthRucksack = "CrZsJsPPZsGzwwsLwLmpwMDw"
+
+        val commonItemInFirstRucksack = findCommonItems(contentOfFirstRucksack)
+        val commonItemInSecondRucksack = findCommonItems(contentOfSecondRucksack)
+        val commonItemInThirdRucksack = findCommonItems(contentOfThirdRucksack)
+        val commonItemInFourthRucksack = findCommonItems(contentOfFourthRucksack)
+        val commonItemInFifthRucksack = findCommonItems(contentOfFifthRucksack)
+        val commonItemInSithRucksack = findCommonItems(contentOfSixthRucksack)
+
+        Assertions.assertThat(commonItemInFirstRucksack).containsOnly('p')
+        Assertions.assertThat(commonItemInSecondRucksack).containsOnly('L')
+        Assertions.assertThat(commonItemInThirdRucksack).containsOnly('P')
+        Assertions.assertThat(commonItemInFourthRucksack).containsOnly('v')
+        Assertions.assertThat(commonItemInFifthRucksack).containsOnly('t')
+        Assertions.assertThat(commonItemInSithRucksack).containsOnly('s')
+    }
+
+    private fun findCommonItems(content: String): Set<Char> {
+        val contentByCompartiment: List<String> = content.chunked(content.length / 2)
+        val contentOfFirstCompartiment = contentByCompartiment.first().toCharArray()
+        val contentOfSecondCompartiment = contentByCompartiment.last().toCharArray()
+        return contentOfFirstCompartiment.intersect(contentOfSecondCompartiment.toList())
+    }
+}
