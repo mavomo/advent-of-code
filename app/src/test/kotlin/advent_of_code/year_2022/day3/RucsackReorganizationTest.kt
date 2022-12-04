2d581eeda1fbf00730ee9adc39215524cc2f9df5
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-12-04 10:51:53 +0100

    day3: sample as list done

--- app/src/test/kotlin/advent_of_code/year_2022/day3/RucsackReorganizationTest.kt
+++ app/src/test/kotlin/advent_of_code/year_2022/day3/RucsackReorganizationTest.kt
@@ -1,10 +1,11 @@
 package advent_of_code.year_2022.day3
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class RucsackReorganizationTest {
+    private val alphabetsInLowerCase = ('a'..'z').toSet()
+    private val alphabetsInUpperCase = ('A'..'Z').toSet()
 
     @Test
     fun `should find the common item in both compartments `() {
@@ -31,30 +32,64 @@ internal class RucsackReorganizationTest {
     }
 
     @Test
-    fun `Should count total priorities given their values`(){
-        val alphabetsInLowerCase = ('a'..'z').toSet()
-        val alphabetsInUpperCase = ('A'..'Z').toSet()
+    fun `Should count total priorities given their values`() {
 
         val lowercaseWithPriorities: Map<Char, Int> = alphabetsInLowerCase.mapIndexed { index, currentItem ->
-           Pair(currentItem, index+1)
+            Pair(currentItem, index + 1)
         }.toMap()
 
         var initialIdx = 26
         val uppercaseWithPriorities: Map<Char, Int> = alphabetsInUpperCase.map { currrentItem ->
             initialIdx++
-            Pair(currrentItem,initialIdx )
+            Pair(currrentItem, initialIdx)
         }.toMap()
 
         val contentOfFirstRucksack = "vJrwpWtwJgWrhcsFMMfFFhFp"
         val commonItemInFirstRucksack = findCommonItems(contentOfFirstRucksack).first()
         var prio = 0
-        if (commonItemInFirstRucksack.isLowerCase()){
+        if (commonItemInFirstRucksack.isLowerCase()) {
             prio += lowercaseWithPriorities.get(commonItemInFirstRucksack)!!
         }
 
         assertThat(prio).isEqualTo(16)
     }
 
+    @Test
+    fun `Should do something`() {
+        val sample = listOf(
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        )
+        var priorities = 0
+        val itemsByPriorities = buildItemsTypeWithPriorities()
+
+        sample.forEach {
+            val commonItem = findCommonItems(it).first()
+            if (commonItem.isLowerCase()) {
+                priorities += itemsByPriorities.first.get(commonItem)!!
+            }else {
+                priorities += itemsByPriorities.second.get(commonItem)!!
+            }
+        }
+        assertThat(priorities).isEqualTo(157)
+    }
+
+    private fun buildItemsTypeWithPriorities(): Pair<Map<Char, Int>, Map<Char, Int>> {
+        val lowercaseWithPriorities: Map<Char, Int> = alphabetsInLowerCase.mapIndexed { index, currentItem ->
+            Pair(currentItem, index + 1)
+        }.toMap()
+
+        var initialIdx = 26
+        val uppercaseWithPriorities: Map<Char, Int> = alphabetsInUpperCase.map { currrentItem ->
+            initialIdx++
+            Pair(currrentItem, initialIdx)
+        }.toMap()
+        return Pair(lowercaseWithPriorities, uppercaseWithPriorities)
+    }
 
     private fun findCommonItems(content: String): Set<Char> {
         val contentByCompartiment: List<String> = content.chunked(content.length / 2)
