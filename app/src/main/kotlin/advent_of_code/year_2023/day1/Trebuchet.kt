609be16e022345d726cd92fa2f07d8113fd30beb
mavomo <michelle.avomo@gmail.com>
2023-12-03 16:05:39 +0100

    feature: part 2 - almost there

--- app/src/main/kotlin/advent_of_code/year_2023/day1/Trebuchet.kt
+++ app/src/main/kotlin/advent_of_code/year_2023/day1/Trebuchet.kt
@@ -4,6 +4,19 @@ import java.util.stream.Stream
 
 class Trebuchet {
     companion object {
+
+        val digitsInLetters = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+
         fun getFirstAndLastCalibrations(calibrationValues: Stream<String>): MutableList<Int> {
             return this.getFirstAndLastCalibrations(calibrationValues.toList().joinToString("\n"))
         }
@@ -14,15 +27,87 @@ class Trebuchet {
                 val allDigits: List<Char> = line.toCharArray().filter { it.isDigit() }
                 var combinationOfFirstAndLast = "${allDigits.first()}"
 
-                if (allDigits.size > 1) {
-                    val lastValue: Char = allDigits.last()
-                    combinationOfFirstAndLast += "$lastValue"
+                combinationOfFirstAndLast += if (allDigits.size > 1) {
+                    "${allDigits.last()}"
                 } else {
-                    combinationOfFirstAndLast += "${allDigits.first()}"
+                    "${allDigits.first()}"
                 }
                 combination.add(combinationOfFirstAndLast.toInt())
             }
             return combination
+        }
+
+         fun getFirstAndLastAppearingDigitsInLetters(
+            calibationsValues: String,
+        ): MutableList<String> {
+
+            val allCalibrationValues: MutableList<String> = mutableListOf()
+            calibationsValues.lines().forEach { line ->
+                val digitsPerLine: MutableList<Pair<Int, String>> = mutableListOf()
+                digitsInLetters.keys.forEach { digitAsLetter ->
+                    val itsAMatch = line.contains(digitAsLetter)
+                    if (itsAMatch) {
+                        val cursor = line.indexOf(digitAsLetter)
+                        val value = line.substring(cursor, cursor + digitAsLetter.length)
+                        digitsPerLine.add(Pair(cursor, value))
+
+                    }
+                }
+                digitsPerLine.sortBy { it.first }
+                val firstCalibrationValue = digitsPerLine.first().second
+                val lastCalibrationValue = digitsPerLine.last().second
+                allCalibrationValues.add("$firstCalibrationValue,$lastCalibrationValue")
+            }
+            return allCalibrationValues
+        }
+
+         fun getValidCalibrationsFromLettersToDigits(calibationsValuesInLetters: String): MutableList<CharSequence> {
+            val calibrationsInFullDigits = mutableListOf<CharSequence>()
+
+            calibationsValuesInLetters.lines().forEach { line ->
+                val values = line.split(",")
+                val firstValue = digitsInLetters[values[0]]
+                val lastValue = digitsInLetters[values[1]]
+
+                val valuesInDigits = "$firstValue,$lastValue"
+                calibrationsInFullDigits.add(valuesInDigits)
+            }
+            return calibrationsInFullDigits
+        }
+
+        fun readValidCalibrationsPerLines(
+            puzzleInput: Stream<String>,
+        ): List<Int> {
+            val combinationForTheLine: MutableList<Int> = mutableListOf()
+            puzzleInput.toList().joinToString("\n").lines().forEach { line ->
+                val mixedDigitsAndTheirPositions: MutableSet<Pair<Int, String>> = mutableSetOf()
+                digitsInLetters.keys.forEach { digitAsLetter ->
+                    val itsAMatch = line.contains(digitAsLetter)
+                    if (itsAMatch) {
+                        val cursor = line.indexOf(digitAsLetter)
+                        val value = line.substring(cursor, cursor + digitAsLetter.length)
+                        val valueAsDigit = digitsInLetters[value]
+                        mixedDigitsAndTheirPositions.add(Pair(cursor, "$valueAsDigit"))
+                    }
+                }
+                val allDigits: List<Char> = line.toCharArray().filter { it.isDigit() }
+                if (allDigits.isNotEmpty()) {
+                    val firstDigit = Pair(line.indexOf(allDigits.first()), "${allDigits.first()}")
+                    mixedDigitsAndTheirPositions.add(firstDigit)
+                    if (allDigits.size > 1) {
+                        val lastDigit = Pair(line.lastIndexOf(allDigits.last()), "${allDigits.last()}")
+                        mixedDigitsAndTheirPositions.add(lastDigit)
+                    } else {
+                        val lastDigit = Pair(line.indexOf(allDigits.first()), "${allDigits.first()}")
+                        mixedDigitsAndTheirPositions.add(lastDigit)
+                    }
+                }
+                val orderedDigits = mixedDigitsAndTheirPositions.sortedBy { it.first }
+                val combinedCalibration = "${orderedDigits.first().second}${orderedDigits.last().second}"
+
+                combinationForTheLine.add(combinedCalibration.toInt())
+            }
+            return combinationForTheLine.toList()
         }
 
     }
--- app/src/test/kotlin/advent_of_code/year_2023/day1/TrebuchetTest.kt
+++ app/src/test/kotlin/advent_of_code/year_2023/day1/TrebuchetTest.kt
@@ -1,6 +1,7 @@
 package advent_of_code.year_2023.day1
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.io.BufferedReader
 import java.io.FileReader
@@ -8,29 +9,177 @@ import java.util.stream.Stream
 
 class TrebuchetTest {
 
-    @Test
-    fun `Should go with the example as spec`() {
-        val calibrationValues = """
+    @Nested
+    inner class Examples {
+        @Test
+        fun `_Part1_ Should get the combination of the first and last calibration and their sum`() {
+            val calibrationValues = """
           1abc2
           pqr3stu8vwx
           a1b2c3d4e5f
           treb7uchet
         """.trimIndent()
 
-        val combination: MutableList<Int> = Trebuchet.getFirstAndLastCalibrations(Stream.of(calibrationValues))
+            val combination: MutableList<Int> = Trebuchet.getFirstAndLastCalibrations(Stream.of(calibrationValues))
 
-        Assertions.assertThat(combination).containsExactly(12, 38, 15, 77)
-        Assertions.assertThat(combination.sum()).isEqualTo(142)
+            Assertions.assertThat(combination).containsExactly(12, 38, 15, 77)
+            Assertions.assertThat(combination.sum()).isEqualTo(142)
+        }
+
+        @Test
+        fun `_Part2_ Should return the first and last digit in full letter given a sequence`() {
+            val calibationsValues = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent()
+
+            val allCalibrationValues = Trebuchet.getFirstAndLastAppearingDigitsInLetters(calibationsValues)
+
+            Assertions.assertThat(allCalibrationValues)
+                .containsExactly(
+                    "two,nine",
+                    "eight,three",
+                    "one,three",
+                    "two,four",
+                    "nine,seven",
+                    "one,eight",
+                    "six,six"
+                )
+        }
+
+        @Test
+        fun `Part_2 Should return the first and last calibration given a sequence with digits in spelled out with letters`() {
+            val calibationsValuesInLetters = """
+            two,nine
+            eight,three
+            one,three
+            two,four
+            nine,seven
+            one,eight
+            six,six
+        """.trimIndent()
+            val calibrationsInFullDigits = Trebuchet.getValidCalibrationsFromLettersToDigits(calibationsValuesInLetters)
+
+            Assertions.assertThat(calibrationsInFullDigits)
+                .containsExactly(
+                    "2,9", "8,3", "1,3", "2,4", "9,7", "1,8", "6,6"
+                )
+        }
+
+        @Test
+        fun `Should treat a whole line with the digits and the spelled in letters ones`() {
+            val sampleInput = """
+                two1nine
+                eightwothree
+                abcone2threexyz
+                xtwone3four
+                4nineeightseven2
+                zoneight234
+                7pqrstsixteen
+            """.trimIndent()
+
+            val combinationForTheLine: List<Int> = Trebuchet.readValidCalibrationsPerLines(Stream.of(sampleInput))
+
+            Assertions.assertThat(combinationForTheLine).containsExactly(
+                29,
+                83,
+                13,
+                24,
+                42,
+                14,
+                76
+            )
+
+            Assertions.assertThat(combinationForTheLine.sum())
+                .isEqualTo(281)
+        }
+
+        @Test
+        fun `Should do something`(){
+            val sample= """
+               eightthree6qskmkzs
+               cmkvmr3srbsnq7onefourbfsrbjvr
+               t3mtgjq4stm84
+               fiveseven2grlmfhmfg8bsb
+               9bgbplvtzstdsevenonedrbxhftrxgmqftjmdrr
+               sixfivezptk6
+               8mftfiveninedgfmtwo9three9
+               pscstrfnrpllhone5fivefourtwo
+               five33
+               4threegfs
+               6sixthreebvq5
+               tsjvdsljzfgfive6threemqjfhrsqkgfznbt
+               six8five
+               twoznvvqgmd5jsxltq
+               zvh9one
+               4threesevenfivesix2hmm
+               l8ccxxhqqjb1qltqxht9qknltdbmdbmone
+               psevenfournine7bzphqxtfmfhsbtxxldhcqj
+               9bzxmpnlqmt8
+               two73nineeighteight
+               5sixseven
+            """.trimIndent()
+
+
+            val combinationForTheLine: List<Int> = Trebuchet.readValidCalibrationsPerLines(Stream.of(sample))
+
+            Assertions.assertThat(combinationForTheLine).containsExactly(
+                86,
+                34,
+                34,
+                58,
+                91,
+                66,
+                89,
+                12,
+                53,
+                43,
+                65,// new start
+                53,
+                65,
+                25,
+                91,
+                42,
+                81,
+                77,
+                98,
+                28,
+                57
+            )
+
+        }
+
+
     }
 
-    @Test
-    fun `_part1_ Should read the puzzle input and compute the sum of calibration values`() {
-        val filePath = "src/test/resources/2023/day1"
-        val calibrationValues: Stream<String> = BufferedReader(FileReader("$filePath/puzzleInput.txt")).lines()
+    @Nested
+    inner class Puzzles {
+        @Test
+        fun `_Part1_ Should read the puzzle input and compute the sum of calibration values`() {
+            val filePath = "src/test/resources/2023/day1"
+            val calibrationValues: Stream<String> = BufferedReader(FileReader("$filePath/puzzleInput.txt")).lines()
 
-        val combinations: MutableList<Int> = Trebuchet.getFirstAndLastCalibrations(calibrationValues)
+            val combinations: MutableList<Int> = Trebuchet.getFirstAndLastCalibrations(calibrationValues)
 
-        Assertions.assertThat(combinations.sum()).isEqualTo(55123)
+            Assertions.assertThat(combinations.sum()).isEqualTo(55123)
+        }
+
+        @Test
+        fun `_Part2_ Should read the puzzle input with the digits and the spelled in letters ones and compute the sum`() {
+            val filePath = "src/test/resources/2023/day1"
+            val calibrationValues: Stream<String> = BufferedReader(FileReader("$filePath/puzzleInput.txt")).lines()
+
+            val combinationForTheLine: List<Int> = Trebuchet.readValidCalibrationsPerLines(calibrationValues)
+
+
+            Assertions.assertThat(combinationForTheLine.sum()).isEqualTo(55297)
+
+        }
     }
 
 
